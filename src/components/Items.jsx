2ef18ed347e30dcbb619fff42a6bd649a6aacfe4
Kofi Osei-Okyere <rhis.cky10@gmail.com>
2024-02-02 03:49:51 +0000

    pizza

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 class Items extends React.Component {
   render() {
-    console.log(this.props);
+    console.log(this.props.imgSrc);
     return <div className="col-md-3">
 	<div className="card" style="width: 18rem;">
-  <img src="cheese.jpg" height="200vh" className="card-img-top" alt="..."><h1>Cheezy Pizza</h1>
+  <img src="cheese.jpg" height="200vh" className="card-img-top" alt="..."><h1>{this.props.title}</h1>
   <div className="card-body">
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
